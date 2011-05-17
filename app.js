804cd4459384351639f56dd1da1c66a2da3938c8
romanmt <romanmt@gmail.com>
2011-05-17 14:46:12 -0400

    select constant numver of values

--- app.js
+++ app.js
@@ -6,22 +6,19 @@ var mongolian  = require('mongolian')
   , Faker      = require('Faker');
 
 var products = db.collection("products");
+var mrresult = db.collection("mrresult");
 
 var map = function() {
-  emit(this.group, 1);
+  emit(this._id, 1);
 };
 
 var reduce = function(k, vals) {
-  var sum = 0;
-  for(var i in vals) {
-    sum += vals[i];
-  }
-  return sum;
+  return 1;
 };
 
 products.drop(function(){
 
-  for(i=0; i < 1000; i++) {
+  for(i=0; i < 10000; i++) {
     products.save(
       { product: Faker.Company.bs()
       , company: Faker.Company.companyName()
@@ -29,13 +26,24 @@ products.drop(function(){
       , group: i % 10
       });   
   };
-  
-  products.mapReduce(map, reduce, {query : { group : 2, random : {$lte: Math.random()}}},
-    function(err, res) {
-      if(err) throw err;
-      inspect(res.find());
-    }
-  );
+
+  var sample = Math.random();
+
+  var plist = function(direction, operation, group, limit) {
+    var options = {limit: limit, query: {random: {}} , out: { } };
+    options.query.random[direction] = sample;
+    options.query.group = group;
+    options.out[operation] = "mrreduce";
+    products.mapReduce(map, reduce, options,
+      function(err, res) {
+        if(err) throw err;
+        if(res.counts.output < limit) {
+          console.log("Not enough data! need %d, got %d", limit, res.counts.output);
+          plist("$gt", "merge", limit - res.counts.output);
+        }
+      });
+    };
+    plist("$lte", "replace", 2, 100);
 });
 
 
